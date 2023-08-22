85102c6cef503e76ef034284ec8e1486c90c25ee
ILYSHD <ilyashaddaoui0@gmail.com>
2023-08-22 22:18:07 +0000

    adding simpled shell project scripts

---
+++ _strcat.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+
+/**
+ * _strcat - function that appends a string to another
+ *
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: returns the pointer of the first string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i])
+	{
+		i++;
+	}
+	while (src[j])
+	{
+		dest[i++] = src[j];
+		j++;
+	}
+	return (dest);
+}
